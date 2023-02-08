9dc0e95534c07da1f5f4d50c3bc19974df420793
Esmay <Molinaesme729@gmail.com>
2023-02-08 10:50:09 -0700

    added button using code

--- test button/ViewController.swift
+++ test button/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redbutton: UIButton!
+    
+    @IBOutlet weak var bluebutton: UIButton!
+    
+    @IBOutlet weak var greenbutton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        redbutton.addTarget(self,action: #selector(buttonpressed(button:)), for: .touchUpInside)
+        
     }
-
-
+    
+    
+    
+    @objc func buttonpressed(button: UIButton){
+    }
 }
-
